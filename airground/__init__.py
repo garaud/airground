114bc8d78e364f35d9bb592a864cb75fee2f81bb
Damien Garaud <damien.garaud@gmail.com>
2016-11-25 17:41:02 +0100

    configure the logger and add some global API variables

--- airground/__init__.py
+++ airground/__init__.py
@@ -2,3 +2,13 @@
 
 """Airground package
 """
+
+import os
+import logging
+
+# API Token
+OPEN_WEATHER_APPID = os.environ.get("OPEN_WEATHER_APPID", "WRONG_KEY")
+DARK_SKY_APPID = os.environ.get("DARK_SKY_APPID", "WRONG_KEY")
+
+LOG_FORMAT = "%(levelname)s :: %(asctime)s :: %(module)s :: %(funcName)s :: %(message)s"
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
